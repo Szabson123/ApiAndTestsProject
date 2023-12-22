912447c35a60c0da165ddd08020160b0b2716260
Szymon Żaba <szymonzaba18@gmail.com>
2023-12-23 00:02:37 +0100

    First step od DB

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,11 +1,12 @@
 from django.test import SimpleTestCase
 from app.calc import add, subtract
 
+
 class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         res = add(5, 6)
         self.assertEqual(res, 11)
 
-    def test_substract_numbers(self):
+    def test_subtract_numbers(self):
         res = subtract(10, 15)
         self.assertEqual(res, 5)
