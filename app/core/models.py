d53a2f849d42e8202565651bc5951bcc3e212de8
Szymon Żaba <szymonzaba18@gmail.com>
2023-12-27 20:29:10 +0100

    recipe models

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,7 @@ from django.db import models # noqa
 from django.contrib.auth.models import (AbstractBaseUser,
                                         BaseUserManager,
                                         PermissionsMixin)
+from django.conf import settings
 
 
 class UserManager(BaseUserManager):
@@ -32,3 +33,16 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Recipe(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    description = models.CharField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
