41f65521bc9b58e5025deb15cd9445c1c2253739
Szymon Żaba <szymonzaba18@gmail.com>
2024-01-08 22:19:12 +0100

    Fixed Lint

--- app/core/tests/test_model.py
+++ app/core/tests/test_model.py
@@ -68,7 +68,8 @@ class ModelTests(TestCase):
 
     def test_create_ingredients(self):
         user = create_user()
-        ingredient = models.Ingredient.objects.create(user=user, name='Ingredient1')
+        ingredient = (models.Ingredient.objects.create
+                      (user=user, name='Ingredient1'))
 
         self.assertEqual(str(ingredient), ingredient.name)
 
